87872230e637027524d75eab04d5ac0a008127b4
rahul-ojha-07 <rahulojha1097+github@gmail.com>
2024-02-13 20:40:59 +0530

    * removed node_modules * get api key from .env * refactoring code & fixing typos

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,21 +1,22 @@
 const request = require('request')
 const chalk = require('chalk')
+require('dotenv').config()
 
-
+const access_token = process.env.ACCESS_TOKEN
 //get the co-ordinates of a city by name
 const geocode = (cityName,callback) => {
     //url of mapbox with api-key
-    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(cityName) + ".json?access_token=pk.eyJ1IjoiY3JhenlnaG91bDA3IiwiYSI6ImNrMHRjZGV2cjBiMWkzZHAzNmx5Mmw4NncifQ.7kKd8_O_iMSbz_QlQ7H0qQ"
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(cityName) + ".json?access_token=" + access_token;
     //making request to the server of mapbox.com
     // console.log(url)
     request({url, "json" : true}, (error,response) =>{
         if (error) { //check for network related 
             // console.log(error);
-            callback('could not coonect to location server',undefined)
+            callback('could not connect to location server',undefined)
             // console.log(chalk.red.inverse.bold("ERROR:") + "Could not connect to server!")
         } else if (response.body.features.length === 0) { //check for bad url
-            // console.log('unable to find loction please try another place')
-            callback('Unable to find location please try again anothr location',undefined)
+            // console.log('unable to find location please try another place')
+            callback('Unable to find location please try again another location',undefined)
         } else { //if no error or issue are there then fetch co-ordinates form response
             // const 
             const body = response.body
